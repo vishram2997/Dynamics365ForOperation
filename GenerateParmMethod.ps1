117a155dcbd6190c21458917b178a8ae3a0d28e3
vishram2997 <vishram2997@gmail.com>
2019-07-28 18:24:04 -0700

    Generate method for X++ in Contract class

---
+++ GenerateParmMethod.ps1
@@ -0,0 +1,33 @@
+﻿
+$tables = import-csv ("C:\Vishram\VW\shipping.csv")
+ForEach ($line in $tables){
+    $line.type +"  " + $line.var +";"
+}
+
+$tables = import-csv ("C:\Vishram\VW\shipping.csv")
+ForEach ($line in $tables){
+    
+    getParmMethod -type $line.type -var $line.var
+
+}
+
+
+
+Function getParmMethod
+{
+    Param ([string]$type,[string]$var)
+    $TextInfo = (Get-Culture).TextInfo
+
+    $method = "[DataMemberAttribute('"+$var+"')]`n"
+    $method += "public "+$type+" parm"+$var.Substring(0,1).ToUpper()+$var.Substring(1,$var.Length-1)+"("+$type+" _"+$var+" = "+$var+")`n"
+    $method += "{`n"
+    $method += "        "+$var+" = _"+$var+";`n"
+    $method += "        return "+$var+";`n"
+    $method += "}`n"
+
+Return $method 
+}
+
+$var = "fontName"
+$type ="str"
+#getParmMethod -type $type -var $var
